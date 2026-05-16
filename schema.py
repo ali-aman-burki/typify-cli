122d1549ee3840b05870f0c1009490c8a2485e7e
Ali Aman <ali.burki1@hotmail.com>
2026-05-16 16:33:45 -0400

    added callsites: []

--- schema.py
+++ schema.py
@@ -20,6 +20,7 @@ class ScopeVisitor(ast.NodeVisitor):
 			entry["goto"] = ""
 		if node_type == "Function":
 			entry["params"] = {p: "" for p in (params or [])}
+			entry["callsites"] = []
 		self.entries[f"{line}:{col}"] = entry
 
 	def visit_Name(self, node: ast.Name) -> None:
